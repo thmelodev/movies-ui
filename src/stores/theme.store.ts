101074a8b038615aa204017087e64b20c74dd79c
Thiago Melo <thiagomelo0509@gmail.com>
2025-10-28 08:07:07 -0300

    feat: init project

---
+++ src/stores/theme.store.ts
@@ -0,0 +1,29 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+type ThemeState = {
+  theme: 'light' | 'dark'
+  toggleTheme: () => void
+
+}
+
+export const useThemeStore = create(
+  persist<ThemeState>(
+    (set, get) => ({
+      theme: 'dark',
+      toggleTheme: () => {
+        const newTheme = get().theme === 'light' ? 'dark' : 'light'
+        set({ theme: newTheme })
+        document.documentElement.classList.toggle('dark', newTheme === 'dark')
+      }
+    }),
+    {
+      name: 'theme-storage',
+      onRehydrateStorage: () => (state) => {
+        if (state) {
+          document.documentElement.classList.toggle('dark', state.theme === 'dark')
+        }
+      }
+    }
+  )
+)
